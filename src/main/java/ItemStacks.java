daa6e893c0f8a0025d58a4ec317dfecdde358bea
yopoyka <workbench.mods@gmail.com>
2020-11-10 12:13:09 +0300

    ItemStacks helper

---
+++ src/main/java/ItemStacks.java
@@ -0,0 +1,23 @@
+public class ItemStacks {
+    /*
+    public static void decrStackInSlot(net.minecraft.inventory.IInventory inv, int slot, int amount) {
+        final net.minecraft.item.ItemStack stack = inv.getStackInSlot(slot);
+        if (stack != null) {
+            stack.stackSize--;
+            if (stack.stackSize < 1) {
+                inv.setInventorySlotContents(slot, null);
+            }
+        }
+    }
+
+    public static void decrStackInSlot(net.minecraft.item.ItemStack[] inv, int slot, int amount) {
+        final net.minecraft.item.ItemStack stack = inv[slot];
+        if (stack != null) {
+            stack.stackSize--;
+            if (stack.stackSize < 1) {
+                inv[slot] = null;
+            }
+        }
+    }
+     */
+}
