9508507ab18d67491e3ae6ec72ed63b64ababedc
yopoyka <workbench.mods@gmail.com>
2020-11-10 12:12:28 +0300

    BasicClassTransformer

---
+++ src/main/java/BasicClassTransformer.java
@@ -0,0 +1,16 @@
+public class BasicClassTransformer {
+    /*
+    protected java.util.Map<String, net.minecraft.launchwrapper.IClassTransformer> transformers = new java.util.HashMap<>();
+
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] basicClass) {
+        if (transformers.isEmpty()) return basicClass;
+
+        final net.minecraft.launchwrapper.IClassTransformer transformer = transformers.remove(transformedName);
+        if (transformer != null)
+            return transformer.transform(name, transformedName, basicClass);
+
+        return basicClass;
+    }
+     */
+}
