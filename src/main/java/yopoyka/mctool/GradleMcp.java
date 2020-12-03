6c34aecfc6e83b2d140d0c149ca95aff33c60ba9
yopoyka <yopoyka@gmail.com>
2020-12-03 13:56:59 +0300

    Fix GradleMcp NPE

--- src/main/java/yopoyka/mctool/GradleMcp.java
+++ src/main/java/yopoyka/mctool/GradleMcp.java
@@ -1,34 +1,22 @@
 package yopoyka.mctool;
 
-import net.minecraft.launchwrapper.Launch;
-
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class GradleMcp implements IMcp {
     public static GradleMcp instance = new GradleMcp();
-    protected File fieldsFile;
-    protected File methodsFile;
-    protected Map<String, String> fieldMap;
-    protected Map<String, String> methodMap;
+    protected java.io.File fieldsFile;
+    protected java.io.File methodsFile;
+    protected java.util.Map<String, String> fieldMap;
+    protected java.util.Map<String, String> methodMap;
 
     {
         try {
-            final Class<?> gradle = Class.forName("net.minecraftforge.gradle.GradleStartCommon", false, Launch.classLoader);
-            final Field csv_dir = gradle.getDeclaredField("CSV_DIR");
+            final Class<?> gradle = Class.forName("net.minecraftforge.gradle.GradleStartCommon", false, net.minecraft.launchwrapper.Launch.classLoader);
+            final java.lang.reflect.Field csv_dir = gradle.getDeclaredField("CSV_DIR");
             csv_dir.setAccessible(true);
-            final File csvDir = (File) csv_dir.get(null);
+            final java.io.File csvDir = (java.io.File) csv_dir.get(null);
             fieldsFile = csvDir.toPath().resolve("fields.csv").toFile();
             methodsFile = csvDir.toPath().resolve("methods.csv").toFile();
         } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
-            if (Boolean.parseBoolean(System.getProperty("yoyoyka.gradlemcp.verbose"))) {
+            if (Boolean.parseBoolean(System.getProperty("yopoyka.gradlemcp.verbose"))) {
                 System.out.println("GradleStartCommon is not loaded!");
                 e.printStackTrace();
             }
@@ -45,30 +33,36 @@ public class GradleMcp implements IMcp {
 
     public String fromSrgMethod(String srgName) {
         if (methodMap == null)
-            methodMap = readLines(methodsFile);
+            if (methodsFile == null)
+                methodMap = java.util.Collections.emptyMap();
+            else
+                methodMap = readLines(methodsFile);
 
         return methodMap.getOrDefault(srgName, srgName);
     }
 
     public String fromSrgField(String srgName) {
         if (fieldMap == null)
-            fieldMap = readLines(fieldsFile);
+            if (fieldsFile == null)
+                fieldMap = java.util.Collections.emptyMap();
+            else
+                fieldMap = readLines(fieldsFile);
 
         return fieldMap.getOrDefault(srgName, srgName);
     }
 
-    protected static Map<String, String> readLines(File file) {
+    protected static java.util.Map<String, String> readLines(java.io.File file) {
         try {
-            final Map<String, String> map = new HashMap<>();
-            final List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+            final java.util.Map<String, String> map = new java.util.HashMap<>();
+            final java.util.List<String> lines = java.nio.file.Files.readAllLines(file.toPath(), java.nio.charset.StandardCharsets.UTF_8);
             lines.forEach(line -> {
                 final String[] split = line.split(",", 3);
                 map.put(split[0], split[1]);
             });
             return map;
-        } catch (IOException e) {
+        } catch (java.io.IOException e) {
             e.printStackTrace();
-            return Collections.emptyMap();
+            return java.util.Collections.emptyMap();
         }
     }
 }
